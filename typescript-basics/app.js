3ea957e8e29bf51868236e3fcedbb0609ed0d7a1
mattpetts <mattpetts23@gmail.com>
2023-04-25 21:01:02 +0100

    array type

--- typescript-basics/app.js
+++ typescript-basics/app.js
@@ -9,6 +9,13 @@
 // Type infered object
 var person = {
     name: 'Matt',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 };
+var favouriteActivities;
+favouriteActivities = ['Sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- typescript-basics/app.ts
+++ typescript-basics/app.ts
@@ -10,7 +10,16 @@
 // Type infered object
 const person = {
     name: 'Matt',
-    age: 30
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
 }
 
+let favouriteActivities: string[];
+favouriteActivities = ['Sports']
+
 console.log( person.name );
+
+for ( const hobby of person.hobbies ) {
+    console.log( hobby );
+    //console.log( hobby.map() ); // ERROR AS TS KNOWS HOBBY IS A STRING!
+}
