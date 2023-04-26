1828f8aff32e8818c36909cae4811fe8cf4e9ae1
mattpetts <mattpetts23@gmail.com>
2023-04-26 13:08:54 +0100

    function types

--- typescript-basics/app.ts
+++ typescript-basics/app.ts
@@ -7,3 +7,19 @@ function printResult ( num: number ): void {
 }
 
 printResult( add( 5, 12 ) )
+
+// this works, but combineValues can be any function, we need it to be one that accepts two number params
+// let combineValues: Function;
+
+// combineValues = add;
+// //combineValues = 5; // this doesn't work as it's not a function
+
+// console.log( combineValues( 8, 8 ) )
+
+// Now combineValues is more securely typed and we can't assign it as a function that would break
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+//combineValues = 5; // this doesn't work as it's not a function
+
+console.log( combineValues( 8, 8 ) )
